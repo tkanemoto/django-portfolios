46211ad49e2f63e007a2b585bc12ca63bcfa9619
Takeshi Kanemoto <tak.kanemoto@gmail.com>
2017-10-03 11:56:35 +0100

    Add an upload handler to compress JPEG files

---
+++ portfolios/uploadhandlers.py
@@ -0,0 +1,30 @@
+import os
+
+from django.conf import settings
+from django.core.files.uploadhandler import MemoryFileUploadHandler
+from PIL import Image
+
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+
+JPEG_QUALITY = getattr(settings, 'JPEG_QUALITY', 70)
+COMPRESSED_TYPES = ['image/jpeg']
+
+
+class CompressImageUploadHandler(MemoryFileUploadHandler):
+    def file_complete(self, file_size):
+        print('content_type : ' + self.content_type)
+        self.file.seek(0)
+        if not self.content_type is None and self.content_type in COMPRESSED_TYPES:
+            newfile = StringIO()
+            img = Image.open(self.file)
+            img.save(newfile, 'JPEG', quality=JPEG_QUALITY)
+            self.file = newfile
+            name, ext = os.path.splitext(self.file_name)
+            self.file_name = '{0}.{1}'.format(name, 'jpg')
+            self.content_type = 'image/jpeg'
+
+        return super(CompressImageUploadHandler, self).file_complete(file_size)
